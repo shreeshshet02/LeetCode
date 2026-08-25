8cc741f039040108daebca010cc07be77857170d
shreeshshet02 <shreeshlshet02@gmail.com>
2026-08-25 21:14:20 +0000

    Solve longest substring problem

---
+++ Strings/longest_substring.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longest = 0
+
+        for i in range(len(s)):
+            seen = []
+            count = 0
+
+            for j in range(i, len(s)):
+                if s[j] in seen:
+                    break
+
+                seen.append(s[j])
+                count += 1
+
+            longest = max(count, longest)
+
+        return longest
